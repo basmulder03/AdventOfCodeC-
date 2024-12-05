a15fb6940c0485b86508ddb8c1df8669f84dc2c8
Bas Mulder <bas.d.mulder@gmail.com>
2024-12-05 15:05:01 +0100

    2015 day 12

--- Solutions/2015/Day12.cs
+++ Solutions/2015/Day12.cs
@@ -1,4 +1,10 @@
-﻿using Core;
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+using Core;
+using Core.DataHelper;
+using Core.StringHelpers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Solutions._2015;
 
@@ -6,11 +12,29 @@ public class Day12 : IDay
 {
     public int Part1(FileStream fileStream)
     {
-        throw new NotImplementedException();
+        var json = fileStream.ReadSingleLine();
+        return json.GetInts().Sum();
     }
 
     public int Part2(FileStream fileStream)
     {
-        throw new NotImplementedException();
+        var jsonString = fileStream.ReadSingleLine();
+        var json = JToken.Parse(jsonString);
+        return GetSum(json);
+    }
+
+    private static int GetSum(JToken json)
+    {
+        if (json.Type == JTokenType.Object && !json.Children<JProperty>().Any(p => p.Value.Type == JTokenType.String && p.Value.ToString() == "red"))
+        {
+            return json.Children<JProperty>().Sum(p => GetSum(p.Value));
+        }
+
+        return json.Type switch
+        {
+            JTokenType.Array => json.Sum(GetSum),
+            JTokenType.Integer => json.Value<int>(),
+            _ => 0
+        };
     }
 }
