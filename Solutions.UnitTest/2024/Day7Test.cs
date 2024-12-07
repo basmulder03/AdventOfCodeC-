21b5efbc79210c0e83e07d9088cb6b6aa0ef532f
= <=>
2024-12-07 06:39:07 +0100

    2024 day 7

---
+++ Solutions.UnitTest/2024/Day7Test.cs
@@ -0,0 +1,47 @@
+﻿using Core.UnitTest;
+using Shouldly;
+using Solutions._2024;
+
+namespace Solutions.UnitTest._2024;
+
+[TestClass]
+public class Day7Test
+{
+    
+    [TestMethod]
+    public void Part1Test1()
+    {
+        var day = new Day7();
+        var data = FileStreamHelper.GetFileStream("""
+                                                  190: 10 19
+                                                  3267: 81 40 27
+                                                  83: 17 5
+                                                  156: 15 6
+                                                  7290: 6 8 6 15
+                                                  161011: 16 10 13
+                                                  192: 17 8 14
+                                                  21037: 9 7 18 13
+                                                  292: 11 6 16 20
+                                                  """);
+        var result = day.Part1(data);
+        result.ShouldBe(3749);
+    }
+    [TestMethod]
+    public void Part2Test1()
+    {
+        var day = new Day7();
+        var data = FileStreamHelper.GetFileStream("""
+                                                  190: 10 19
+                                                  3267: 81 40 27
+                                                  83: 17 5
+                                                  156: 15 6
+                                                  7290: 6 8 6 15
+                                                  161011: 16 10 13
+                                                  192: 17 8 14
+                                                  21037: 9 7 18 13
+                                                  292: 11 6 16 20
+                                                  """);
+        var result = day.Part2(data);
+        result.ShouldBe(11387);
+    }
+}
---
+++ Solutions/2024/Day7.cs
@@ -0,0 +1,81 @@
+﻿using Core;
+using Core.DataHelper;
+using Core.StringHelpers;
+
+namespace Solutions._2024;
+
+public class Day7 : IDay
+{
+    public long Part1(FileStream fileStream)
+    {
+        var lines = fileStream.ReadLines();
+        var total = 0L;
+
+        foreach (var line in lines)
+        {
+            var split = line.Split(": ");
+            var sum = long.Parse(split[0]);
+            var nums = split[1].GetLongs();
+
+            var totals = new List<long> { nums[0] };
+
+            foreach (var num in nums.Skip(1))
+            {
+                var newTotals = new List<long>();
+                foreach (var totalValue in totals)
+                {
+                    newTotals.Add(Mult(totalValue, num));
+                    newTotals.Add(Sum(totalValue, num));
+                }
+                totals = newTotals;
+            }
+
+            if (totals.Contains(sum))
+            {
+                total += sum;
+            }
+        }
+
+        return total;
+    }
+
+    public long Part2(FileStream fileStream)
+    {
+        var lines = fileStream.ReadLines();
+        var total = 0L;
+
+        foreach (var line in lines)
+        {
+            var split = line.Split(": ");
+            var sum = long.Parse(split[0]);
+            var nums = split[1].GetLongs();
+
+            var totals = new List<long> { nums[0] };
+
+            foreach (var num in nums.Skip(1))
+            {
+                var newTotals = new List<long>();
+                foreach (var totalValue in totals)
+                {
+                    newTotals.Add(Mult(totalValue, num));
+                    newTotals.Add(Sum(totalValue, num));
+                    newTotals.Add(Combine(totalValue, num));
+                }
+                totals = newTotals;
+            }
+
+            if (totals.Contains(sum))
+            {
+                total += sum;
+            }
+        }
+
+        return total;
+    }
+
+    private static long Mult(long a, long b) => a * b;
+
+    private static long Sum(long a, long b) => a + b;
+
+    private static long Combine(long a, long b) => long.Parse($"{a}{b}");
+}
